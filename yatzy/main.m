c335a17ae5e7eb08530b939d8b804edeb56ee474
jerzky <jensg90@gmail.com>
2020-04-28 10:02:31 +0200

    added comments

--- yatzy/main.m
+++ yatzy/main.m
@@ -1,4 +1,4 @@
 clc;
 close all
-[xhat, s2hat] = yatzy(100000, false);
+[xhat, s2hat] = yatzy(10000, false);
 fprintf('The mean is: %.2f and the variance: %.2f\n', xhat, s2hat);
--- yatzy/yatzy.m
+++ yatzy/yatzy.m
@@ -1,3 +1,8 @@
+%Function: The main function of yatzy. Will simulate yatzy throw
+%aswell as calculate a normalcurve and plot that.
+%Paramaters: count : int, the number of throws to simulate
+%            debug : boolean, will print debug messages
+%Return value: Two values are returned [mean variance]
 function [data_mean, data_variance] = yatzy(count, debug)  
     %Perform the MonteCarlo simulation
     data = monteCarlo(count, debug);
@@ -16,7 +21,9 @@ function [data_mean, data_variance] = yatzy(count, debug)
     ylabel('Frequency');
     hold off;
 end
-%This function will calculate the normal for getting a yatzy.
+%Function: This function will calculate the normal for getting a yatzy.
+%Paramaters: count : int, the range of the curve
+%Return value: A matrix of the data for the curve
 function result = normal(count)
     A = [0  (1/6)   (1/36)  (1/216)     (1/1296);
          0  (5/6)   (10/36) (15/216)    (25/1296);
@@ -31,9 +38,12 @@ function result = normal(count)
      end
 end
 
-
-%This function will perfrom a Monte Carlo simulation, count is the number 
-%of times it will perform the simulation
+%Function: This function will perfrom a Monte Carlo simulation, count is 
+%the number of times it will perform the simulation
+%Paramaters: count : int, how many throws to simulate 
+%            debug : boolean, will print debug messages
+%Return value: return a matrix of all the throws and how many throws it
+%took to get a yatzy
 function returnValue = monteCarlo(count, debug)  
     returnValue = zeros(1, count);    
     fprintf('Simulating %d yatzy rounds\n', count);  
@@ -45,9 +55,8 @@ function returnValue = monteCarlo(count, debug)
     fprintf('The time it took to run the simulation was: %.1f seconds\n\n', cputime - startTime);
 end
 
-
-
-%Will throw the dies until it gets five of a kind.
+%Function: Will throw the dies until it gets five of a kind.
+%Paramaters: debug : boolean, will print debug messages
 function throwCount = fiveDies(debug)
     current = [];
     %First throw, so we start at 0 throws and 5 dies.
@@ -69,9 +78,10 @@ function throwCount = fiveDies(debug)
     end
 end
 
-
-
-%Simulates die throws, count is the number of dies thrown.
+%Function: Simulates die throws, count is the number of dies thrown.
+%Paramaters: count : will return a 1x6 matrix with random die throws
+%            debug : boolean, will print debug messages
+%Return value: a 1x6 matrix of dies
 function returnValue = simulateThrowDie(count, debug)
     returnValue = randi([1 6], 1, count);   
     if(debug)
@@ -80,8 +90,10 @@ function returnValue = simulateThrowDie(count, debug)
     end
 end
 
-%This will sum up how many of each die there is, and returns an array of
-%how how many of each
+%Function: This will sum up how many of each die there is, and returns an array of
+%how how many of each 
+%Paramaters: 1x6 integer matrix : A matrix of dies thrown
+%Return value: Will return an 1x6 matrix of the occurence of dies
 function count = countOutcome(values)
     count = zeros(1, 6);
     for i = 1:length(values)
@@ -89,16 +101,23 @@ function count = countOutcome(values)
     end
 end
 
-%This function will check what die is the most common
+%Function: This function will check what die is the most common
 %and returns what value was most common and how many there was.
+%Paramaters: 1x6 integer matrix : A matrix of dies thrown
+%Return value: Will return the die that was most common and how the count
+%of that die
 function returnValue = commonOutcome(values)
     outcomes = countOutcome(values);
     [count, value] = max(outcomes);
     returnValue = [count value];
 end
 
-%This function will check what dies was most common then it will save those
+%Function: This function will check what dies was most common then it will save those
 %in an array and return only those.
+%Paramaters: An array of size 1-6, with the dies. 
+%            debug : boolean, will print debug messages
+%Return value: An array with the most common dies alone.
+
 function returnValue = findDie(values, debug)    
     outcome = commonOutcome(values);
     count = outcome(1);
